32571af7e4f5bee2c896d455ae9b304723ffd6e1
cyrustheone <rambhushan69@gmail.com>
2021-07-31 12:16:43 +0000

    added sonar-project.js file

---
+++ sonar-project.js
@@ -0,0 +1,17 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+
+sonarqubeScanner({
+  serverUrl: 'http://3.109.1.48:9000/',
+       options : {
+	    'sonar.projectDescription': 'This is a sample Node JS application',
+	    'sonar.projectName': 'Sample Node JS Application',
+	    'sonar.projectKey':'SampleNodeJs',
+	    'sonar.login': '54b37cd8a347eaae59f3aa1ec1ebe8909ce3b97a',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+}, () => {});
