f17fefe2534c4b4b30e3982e11336cadd90be5c9
cryptocyrus69 <73691007+cryptocyrus69@users.noreply.github.com>
2021-11-28 08:50:12 +0530

    Create demo.ps1

---
+++ demo.ps1
@@ -0,0 +1,11 @@
+New-LocalUser "ansible" -Password (ConvertTo-SecureString -AsPlainText -Force 'Testing@123') -AccountNeverExpires:$true -PasswordNeverExpires:$true -FullName "ansible" | Add-LocalGroupMember -Group administrators
+
+
+
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+# $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+
+# (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+
+# powershell.exe -ExecutionPolicy ByPass -File $file
