430ab17bf13c203da667de15f71685582007c606
cryptocyrus69 <73691007+cryptocyrus69@users.noreply.github.com>
2021-12-02 16:27:45 +0530

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -2,12 +2,12 @@ New-LocalUser "ansible" -Password (ConvertTo-SecureString -AsPlainText -Force 'T
 
 
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+# $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+# (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 
-powershell.exe -ExecutionPolicy ByPass -File $file
+# powershell.exe -ExecutionPolicy ByPass -File $file
 
-Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
+# Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
