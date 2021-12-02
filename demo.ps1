ffbbdd3a6ecd4680dfc43d0e1e5a55230effbc47
cryptocyrus69 <73691007+cryptocyrus69@users.noreply.github.com>
2021-12-02 09:46:25 +0530

    Update demo.ps1

--- demo.ps1
+++ demo.ps1
@@ -1,13 +1,13 @@
-New-LocalUser "ansible" -Password (ConvertTo-SecureString -AsPlainText -Force ) -AccountNeverExpires:$true -PasswordNeverExpires:$true -FullName "ansible" | Add-LocalGroupMember -Group administrators
+# New-LocalUser "ansible" -Password (ConvertTo-SecureString -AsPlainText -Force ) -AccountNeverExpires:$true -PasswordNeverExpires:$true -FullName "ansible" | Add-LocalGroupMember -Group administrators
 
 
 
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+# [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+# $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+# $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+# (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
 
-powershell.exe -ExecutionPolicy ByPass -File $file
+# powershell.exe -ExecutionPolicy ByPass -File $file
 
 Install-WindowsFeature -Name Web-Server -IncludeAllSubFeature -IncludeManagementTools
